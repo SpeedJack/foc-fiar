c2d341255635a066de0575a8e5ef2dc09931b3c3
Niccolò Scatena <speedjack95@gmail.com>
2020-05-05 18:48:07 +0200

    Add comments to list module

--- common/list.c
+++ common/list.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include "list.h"
 
+/* Adds a new entry between two entries. */
 static inline void __list_add(struct list_head *new,
 			struct list_head *prev,
 			struct list_head *next)
@@ -11,34 +12,34 @@ static inline void __list_add(struct list_head *new,
 	prev->next = new;
 }
 
-void list_add(struct list_head *new, struct list_head *head)
-{
-	__list_add(new, head, head->next);
-}
-
-void list_add_tail(struct list_head *new, struct list_head *head)
-{
-	__list_add(new, head->prev, head);
-}
-
+/* Removes the entry between the two specified entries. */
 static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
 	next->prev = prev;
 	prev->next = next;
 }
 
-static inline void __list_del_entry(struct list_head *entry)
+/* Adds an entry to the list, after the specified list head. */
+void list_add(struct list_head *new, struct list_head *head)
 {
-	__list_del(entry->prev, entry->next);
+	__list_add(new, head, head->next);
 }
 
+/* Adds an entry to the list, before the specified list head. */
+void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
+
+/* Removes the specified entry from the list. */
 void list_del(struct list_head *entry)
 {
-	__list_del_entry(entry);
+	__list_del(entry->prev, entry->next);
 	entry->next = NULL;
 	entry->prev = NULL;
 }
 
+/* Counts the number of entries in the list. */
 int list_count(const struct list_head *head)
 {
 	int count = 0;
@@ -47,3 +48,4 @@ int list_count(const struct list_head *head)
 		count++;
 	return count;
 }
+
--- include/list.h
+++ include/list.h
@@ -3,63 +3,98 @@
 
 #include <stdbool.h>
 
+/* Doubly linked list implementation. */
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+/* Defines a new list with the specified name. */
 #define LIST_HEAD(name) \
 	struct list_head name = LIST_HEAD_INIT(name)
 
+/* Initializes an empty list (with just one element). */
 #define LIST_HEAD_INIT(name) { &(name), &(name) }
 
+/*
+ * Returns the entry that contains the specified list head. type is the type to
+ * return; member is the name of the list head member in the structure.
+ */
 #define list_entry(ptr, type, member) ({			\
 	const struct list_head *__lptr = (ptr);	\
 	(type *)( (char *)__lptr - offsetof(type, member) ); })
 
+/*
+ * Returns the first element of the list specified by ptr. type is the type to
+ * return; member is the name of the list head member in the structure.
+ */
 #define list_first_entry(ptr, type, member) \
 	list_entry((ptr)->next, type, member)
 
+/*
+ * Returns the last element of the list specified by ptr. type is the type to
+ * return; member is the name of the list head member in the structure.
+ */
 #define list_last_entry(ptr, type, member) \
 	list_entry((ptr)->prev, type, member)
 
+/*
+ * Returns the next element in the list. pos is a pointer to the current
+ * element; member is the name of the list head member in the structure.
+ */
 #define list_next_entry(pos, member) \
 	list_entry((pos)->member.next, typeof(*(pos)), member)
 
+/*
+ * Returns the previous element in the list. pos is a pointer to the current
+ * element; member is the name of the list head member in the structure.
+ */
 #define list_prev_entry(pos, member) \
 	list_entry((pos)->member.prev, typeof(*(pos)), member)
 
+/*
+ * Do a for-each over all list heads of the list. pos is a pointer to the
+ * current list head; head is a pointer to the list head of the first element.
+ */
 #define list_for_each(pos, head) \
 	for (pos = (head)->next; pos != (head); pos = pos->next)
 
+/*
+ * Do a for-each over all elements of the list. pos is a pointer to the current
+ * element; head is a pointer to the list head of the first element; member is
+ * the namne of the list head member in the structure.
+ */
 #define list_for_each_entry(pos, head, member)				\
 	for (pos = list_first_entry(head, typeof(*pos), member);	\
 		&pos->member != (head);					\
 		pos = list_next_entry(pos, member))
 
-struct list_head {
-	struct list_head *next, *prev;
-};
-
+/* Initializes an empty list head. */
 static inline void INIT_LIST_HEAD(struct list_head *list)
 {
 	list->next = list;
 	list->prev = list;
 }
 
-
 extern void list_add(struct list_head *new, struct list_head *head);
 extern void list_add_tail(struct list_head *new, struct list_head *head);
 extern void list_del(struct list_head *entry);
 extern int list_count(const struct list_head *head);
 
+/* Checks if the specified list head is the first in the list. */
 static inline bool list_is_first(const struct list_head *list,
 			const struct list_head *head)
 {
 	return list->prev == head;
 }
 
+/* Checks if the specified list head is the last in the list. */
 static inline bool list_is_last(const struct list_head *list,
 			const struct list_head *head)
 {
 	return list->next == head;
 }
 
+/* Checks if the specified list is empty. */
 static inline bool list_empty(const struct list_head *head)
 {
 	return head->next == head;

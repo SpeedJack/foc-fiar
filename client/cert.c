437fe2520ce51961c805e46a0a0afb728e9fa8dc
Niccolò Scatena <speedjack95@gmail.com>
2020-06-18 12:29:50 +0200

    Fix code style

--- client/cert.c
+++ client/cert.c
@@ -1,5 +1,3 @@
-//#include "file.h"
-//#include "sign.h"
 #include <stdio.h>
 #include <string.h>
 #include <openssl/hmac.h>
@@ -12,43 +10,44 @@
 
 bool cert_init(const char *ca_filename, const char *crl_filename, const char *cert_filename)
 {
-  X509* ca;
+	X509* ca;
 	FILE *fp = fopen(ca_filename, "r");
 	ca = PEM_read_X509(fp, NULL, NULL, NULL);
 	fclose(fp);
 
-  X509* cert;
-  fp = fopen(cert_filename, "r");
-  if(!fp) puts("Error");
-  cert = PEM_read_X509(fp, NULL, NULL, NULL);
-  fclose(fp);
+	X509* cert;
+	fp = fopen(cert_filename, "r");
+	if(!fp)
+		puts("Error");
+	cert = PEM_read_X509(fp, NULL, NULL, NULL);
+	fclose(fp);
 
-  X509_CRL *crl;
-  fp = fopen(crl_filename, "r");
-	if(!(fp)) puts("Error");
+	X509_CRL *crl;
+	fp = fopen(crl_filename, "r");
+	if(!fp)
+		puts("Error");
 	crl = PEM_read_X509_CRL(fp, NULL, NULL, NULL);
 	fclose(fp);
 
-  X509_STORE *store = X509_STORE_new();
-  if (!store) puts("Error");
+	X509_STORE *store = X509_STORE_new();
+	if (!store)
+		puts("Error");
 	int ret = X509_STORE_add_cert(store, ca);
-  if(ret!=1) puts("Error");
+	if (ret != 1)
+		puts("Error");
 	ret = X509_STORE_add_crl(store, crl);
-  if(ret!=1) puts("Error");
+	if (ret != 1)
+		puts("Error");
 	ret = X509_STORE_set_flags(store, X509_V_FLAG_CRL_CHECK);
-  if(ret!=1) puts("Error");
+	if (ret != 1)
+		puts("Error");
 
-  X509_STORE_free(store);
+	X509_STORE_free(store);
 
-
-  X509_STORE_CTX *ctx = X509_STORE_CTX_new();
-
-  X509_STORE_CTX_init(ctx, store, cert, NULL);
+	X509_STORE_CTX *ctx = X509_STORE_CTX_new();
+	X509_STORE_CTX_init(ctx, store, cert, NULL);
 	int ret2 = X509_verify_cert(ctx);
-	if (ret2 != 1)
-		return true;
-	else
-		return false;
 	X509_STORE_CTX_free(ctx);
+	return ret2 != 1;
 
 }
